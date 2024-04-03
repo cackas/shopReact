8080fa5e6dff05abf8cb39113664ea373457338a
Ivan <ivprok@localhost.localdomain>
2024-04-03 20:42:35 +0300

    deleteOrder + sum

--- src/components/header.js
+++ src/components/header.js
@@ -8,12 +8,16 @@ export default function Header(props) {
 
   let [cartOpen,setCartOpen] = useState(false)
 
+
   const showOrders = (props) => {
+    let sum = 0
+    props.orders.forEach(el => sum += Number.parseFloat(el.price))
     return (
       <div>
         {props.orders.map(el => (
-          <Order key={el.id} item={el} />
+          <Order onDelete={props.onDelete} key={el.id} item={el} />
         ))}
+        <p className='sum'>Сумма {new Intl.NumberFormat().format(sum)}$</p>
       </div>
     )
   } 
